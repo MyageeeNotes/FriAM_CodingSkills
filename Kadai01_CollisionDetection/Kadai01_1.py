d3005a792a21407c5697862f89078c3c482fb0e4
MyageeeNotes <bgdm1260263@gn.iwasaki.ac.jp>
2019-06-28 14:17:48 +0900

    Versiion 2.0

--- Kadai01_CollisionDetection/Kadai01_1.py
+++ Kadai01_CollisionDetection/Kadai01_1.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     kadai_data = "100 100 70 100\n3\n50 60 100 50\n10 120 100 50\n165 115 70 70"
 
     p, e_max, enms = ColDct.initialize(kadai_data)
-    ColDct.judge(p, e_max, enms)
+    ColDct.alert(p, e_max, enms)
---
+++ Kadai01_CollisionDetection/Kadai01_2.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# Coding Skills: Kadai01
+# 345(29)
+
+from FriAM_CodingSkills.Kadai01_CollisionDetection import Kadai01 as ColDct
+
+
+data = "1000 1000 400 600\n3\n200 1300 830 390\n370 1300 830 390\n500 900 830 390"
+p, e_max, enms = ColDct.initialize(data)
+
+pimg, pw, ph = ColDct.load_image('images/rocket.png')
+eimg, ew, eh = ColDct.load_image('images/enemy.png')
+
+for i in range(e_max):
+    if ColDct.pro_judge(p, enms[i], pimg, eimg):
+        print('敵機 {} が当たり'.format(i + 1))
