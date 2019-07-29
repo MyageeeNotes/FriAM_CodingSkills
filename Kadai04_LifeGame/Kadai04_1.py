24e1d774e4fce3a368c86e6ee3f4b81e6569772a
MyageeeNotes <bgdm1260263@gn.iwasaki.ac.jp>
2019-07-29 10:47:54 +0900

    Version 2

--- Kadai04_LifeGame/Kadai04_1.py
+++ Kadai04_LifeGame/Kadai04_1.py
@@ -3,168 +3,203 @@
 import pygame
 from pygame.locals import *
 import sys
-import Math
+import random
+import time
+import math
+
+# Field info.
+field = {
+    'size': (720, 720),
+    'x': 120,
+    'y': 120,
+    'screen': None,
+    'lifes': [],
+    'init': None,
+    'random': 1000
+}
+# Frame info.
+frame = {
+    'now': 0,
+    'finish': 2019,
+    'speed': 50
+}
 
 if __name__ == '__main__':
+
+    # Drawing
+    def mapping(axis_x, axis_y, flag):
+        size = {
+            'x': field['size'][0] / field['x'],
+            'y': field['size'][1] / field['y'],
+        }
+        if flag == 2:
+            pygame.draw.rect(
+                field['screen'],
+                (200, 100, 100),
+                Rect(
+                    axis_x * size['x'] + 1,
+                    axis_y * size['y'] + 1,
+                    size['x'] - 1, size['y'] - 2
+                ))
+        else:
+            if flag:
+                pygame.draw.rect(
+                    field['screen'],
+                    (200, 200, 200),
+                    Rect(
+                        axis_x * size['x'] + 1,
+                        axis_y * size['y'] + 1,
+                        size['x'] - 1, size['y'] - 2
+                    ))
+            else:
+                pygame.draw.rect(
+                    field['screen'],
+                    (60, 60, 60),
+                    Rect(
+                        axis_x * size['x'] + 1,
+                        axis_y * size['y'] + 1,
+                        size['x'] - 1, size['y'] - 2
+                    ))
+
+
     # SET UP -----
     pygame.init()
-    screen = pygame.display.set_mode((900, 900))
+    field['screen'] = pygame.display.set_mode(field['size'])
     pygame.display.set_caption("Life Game - Kadai0205")
-
-    # Field info.
-    field = {
-        'x': 10,
-        'y': 10,
-        'lifes': [],
-        'init': None,
-        'random': 80
-    }
-    # Frame info.
-    frame = {
-        'now': 0,
-        'finish': 2019,
-        'speed': 10
-    }
 
     # 1.CREATE MAP DATA
     # -- Create bool list
-    def initWorld(randFlag=0):
+    def init_world(rand_flag=0):
         # Initialize Field life list
-        field.init = [False * field.x * field.y]
-        fieldLen = len(field.init)
+        field['init'] = [False] * (field['x'] * field['y'])
+        field_len = len(field['init'])
 
-        # Manual(randFlag=0) or Random lifes > 0
-        if randFlag > 0:
+        # Manual(rand_flag=0) or Random lifes > 0
+        if rand_flag > 0:
             # Field ID list
-            arr = range(fieldLen)
+            arr = list(range(field_len))
             # set Random field to give life
-            l = []
-            for i in range(randFlag):
-                num = Math.floor(Math.random() * arr.length)
-            list.push(arr[num])
-            arr.splice(num, 1)
+            lst = []
+            for i in range(rand_flag):
+                num = random.randint(0, len(arr) - 1)
+                lst.append(arr[num])
+                del arr[num]
 
             # set field info.
-            for i in range(len(l)):
-                field.init[l[i]] = True
+            for i in lst:
+                field['init'][i] = True
 
     # 2. VISUALIZATION
     # Create HTML tags with 1.map data.
-    def createWorld():
+    def create_world():
         cnt = 0
-        for y in range(field.y):
-            tr = $('<tr></tr>')
-        for x in range(field.x):
-            td = new Life([x, y], field.init[cnt])
-            tr.append(td.elem)
-            field['lifes'].push(td)
-            cnt += 1
-
-        $('#field').append(tr)
+        for y in range(field['y']):
+            for x in range(field['x']):
+                mapping(x, y, field['init'][cnt])
+                td = Life([x, y], field['init'][cnt])
+                field['lifes'].append(td)
+                cnt += 1
 
 
     class Life:
         def __init__(self, pos, life):
-            self.pos = pos;
-            self.life = life;
-            self.elem = $('<td></td>');
-            self.elem.attr('id', 'x' + pos[1] + 'y' + pos[0]);
-            self.elem.attr('onclick', 'godhand(' + pos[1] + ',' + pos[0] + ');');
-            self.alives = 0;
+            self.pos = pos
+            self.life = life
+            self.alives = 0
             self.center = {
                 'x': self.pos[0],
                 'y': self.pos[1]
             }
-            if self.life:
-                self.elem.addClass('alive')
-            else:
-                self.elem.addClass('dead')
 
-            # Toggle life
-            def godKnows():
-                if self.life:
-                    self.life = false
-                else:
-                    self.life = true
-                    self.setVessel(self.life)
-
-        def spend():
+        def spend(self):
             # Neighbors alives
-            self.alives = 0;
-            # Left neighbor to Right neighbor[■□□ -> □□■]
-            for i in range(3):
-                px = self.center.x - 1 + i
-                # Is target[x] in field?
-                if px >= 0 and px < field.x:
-                    # Above neighbor to Bottom neighbor
-                    for j in range(3):
-                        py = self.center.y - 1 + i
-                        # Is target[y] in field?
-                        if(py >= 0 and py < field.y:
-                            # Is target alive?
-                            if (self.getVessel([px, py]).hasClass('alive')):
-                                # target is not myself?
-                                if not (px == self.center.x and py == self.center.y):
-                                    self.alives += 1
+            self.alives = 0
+            for i in range(9):
+                px = self.center['x'] - 1 + math.floor(i % 3)
+                py = self.center['y'] - 1 + math.floor(i / 3)
+                # Is target in field?
+                in_field_x = 0 <= px < field['x']
+                in_field_y = 0 <= py < field['y']
+                # Is not Myself
+                not_myself = not (px == self.center['x'] and py == self.center['y'])
+                if in_field_x and in_field_y and not_myself:
+                    # Is target alive?
+                    if self.get_vessel([px, py]).life:
+                        self.alives += 1
             # DEAD or ALIVE
             if self.alives < 2 or 3 < self.alives:
                 return False
             elif self.alives == 3:
                 # BIRTH
-                return true
+                return True
             else:
                 return self.life
 
-        # Return HTML oject
-        def getVessel(pos):
-            return $("#field tr:nth-child(" + (pos[1] + 1) + ") td:nth-child(" + (pos[0] + 1) + ")")
+        # Return oject
+        def get_vessel(self, pos):
+            return field['lifes'][pos[0] + pos[1] * field['x']]
 
         # Change class
-        def setVessel(flag):
-            target = self.getVessel(self.pos)
-            if flag:
-                target.addClass('alive')
-                target.removeClass('dead')
-            else:
-                target.addClass('dead')
-                target.removeClass('alive')
+        def set_vessel(self, flag):
+            target = self.get_vessel(self.pos)
+            target.life = flag
 
         # Manual set
-        def godhand(x, y):
-            target = field.lifes[(x * field.x) + y]
-            target.godKnows()
+        def god_hand(self):
+            self.life = not self.life
+            field['init'][self.pos[0] + self.pos[1] * field['x']] = self.life
 
     # Judge life
     def spend():
-        frame.now += 1
-        $("#turn").text(frame.now + ' years')
-        for i in range(len(field.lifes)):
-            field.init[i] = field.lifes[i].spend()
-        for i in range(len(field.lifes)):
-            field.lifes[i].setVessel(field.init[i])
+        frame['now'] += 1
+        for i in range(len(field['lifes'])):
+            field['init'][i] = field['lifes'][i].spend()
+        for i in range(len(field['lifes'])):
+            field['lifes'][i].set_vessel(field['init'][i])
+        cnt = 0
+        for y in range(field['y']):
+            for x in range(field['x']):
+                mapping(x, y, field['init'][cnt])
+                cnt += 1
 
     # ------------------------------------------------------------------
     # set Playing stop
-    playing_stat = False
-    initWorld(field.random)
-    createWorld()
+    playing_stat = True
+    init_world(field['random'])
+    create_world()
 
     while True:
+        # Renew background image
+        field['screen'].fill((50, 50, 50))
+
         if playing_stat:
             spend()
         # FINISH
-        if frame.now == frame.finish:
-            clearInterval(start)
-            console.log("end")
+        if frame['now'] == frame['finish']:
+            print("Finish -- Max Frames: {}".format(frame['finish']))
+            break
 
-        # Renew background image
-        screen.fill((0, 0, 0))
         pygame.display.update()
 
         # Events
         for event in pygame.event.get():
-
             # Close window
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            # Keyboard event
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                x, y = event.pos
+                size = {
+                    'x': field['size'][0] / field['x'],
+                    'y': field['size'][1] / field['y'],
+                }
+                size['x'] - 1, size['y'] - 2
+                px = math.floor(x / size['x'])
+                py = math.floor(y / size['y'])
+                field['lifes'][px + py * field['x']].god_hand()
+
+        time.sleep(frame['speed'] / 1000)
