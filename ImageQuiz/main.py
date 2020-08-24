07d0a0ad94868545de537cf75e85404638d6db2e
MyageeeNotes <daigorian@miyajima.ddo.jp>
2020-08-24 11:25:56 +0900

    一旦UP

--- ImageQuiz/main.py
+++ ImageQuiz/main.py
@@ -1,73 +1,91 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import Canvas
-from tkinter import NW
 from PIL import Image, ImageTk
-import tkinter.font as tkFont
+
+# 専用ライブラリ
+from ImageQuiz import mondaishu
 
 
-def check(event):
-    if answer.get() == '北海道':
-        print("正解！！")
+def getHint(event, data):
+    print(hint1)
+
+
+def setAnswer(event, mondai):
+    input = textarea.get()
+    textarea.delete(0, END)
+
+    if input == mondai.data[4]:
+        textarea.insert(0, "正解！！")
+        textarea.insert(END, "入力した答え：" + input)
     else:
-        print("ざんねん、正解は「{}」でした！".format('北海道'))
+        textarea.insert(0, "ざんねん、正解は「" + mondai.data[4] + "」でした！")
+        textarea.insert(END, "入力した答え：" + input)
 
 
+def updateQuestion(event, mondai):
+    mondai.next()
+    # 問題文をセット
+    question["text"] = "問題：" + mondai.data[0]
+    # ヒントをセット
+    hint1["text"] = "ヒント1: " + mondai.data[1]
+    hint2["text"] = "ヒント1: " + mondai.data[2]
+    # 画像をセット
+    img = Image.open("image/" + mondai.data[3])
+    img = img.resize((400, 400))
+    img = ImageTk.PhotoImage(img)
+    picture["image"] = img
+
+
+# メインウィンドウの描画
 root = Tk()
 root.title('都道府県クイズアプリ')
+root.resizable(width=False, height=False)
 root.geometry("640x640")
 
 # フレームの作成
-area_question = ttk.Frame(root, padding=10)
-area_image = ttk.Frame(root, padding=10)
-area_hints = ttk.Frame(root, padding=10)
-area_button = ttk.Frame(root, padding=10)
-area_text = ttk.Frame(root, padding=10)
-area_answer = ttk.Frame(root, padding=10)
+question_frame = ttk.Frame(root, padding=10)
+text_frame = ttk.Frame(root, padding=10)
+answer_frame = ttk.Frame(root, padding=10)
 
 with open('question.txt', encoding='utf-8') as file:
-    data = file.readlines()
+    file = file.read().splitlines()
+    mondai = mondaishu.Mondaishu(file)
 
-q, im, h1, h2, h3, ans = data[0].split(',')
-question = ttk.Label(area_question, font=("", 20), text="問題：" + q)
-hint1 = ttk.Label(area_hints, font=("", 16), text="ヒント1: " + h1)
-hint2 = ttk.Label(area_hints, font=("", 16), text="ヒント2: " + h2)
-hint3 = ttk.Label(area_hints, font=("", 16), text="ヒント3: --")
+# ラベルの作成
+question = ttk.Label(question_frame, font=("", 16), padding=10)
+hint1 = ttk.Label(question_frame, font=("", 12), padding=5)
+hint2 = ttk.Label(question_frame, font=("", 12), padding=5)
+picture = ttk.Label(question_frame)
 
-# 画像表示
-img = Image.open("image/" + im)
-img = img.resize((400, 240))
-img = ImageTk.PhotoImage(img)
-
-# Label 1
-picture = ttk.Label(area_image, image=img)
-
+# 答え入力欄の作成
 t = StringVar()
-answer = ttk.Entry(area_text, font=("", 20), textvariable=t)
-button1 = ttk.Button(area_answer, text='ヒントを出す')
-button2 = ttk.Button(area_answer, text='答える！')
-button1.bind("<Button-1>", check)
-button2.bind("<Button-1>", check)
+textarea = ttk.Entry(answer_frame, font=("", 16), width=640, textvariable=t)
+
+# ボタンの作成
+button1 = ttk.Button(answer_frame, padding=10, text='ヒントを出す')
+button2 = ttk.Button(answer_frame, padding=10, text='答える！')
+button3 = ttk.Button(answer_frame, padding=10, text='次の問題')
+button1.bind("<Button-1>", getHint(data=mondai))
+button2.bind("<Button-1>", setAnswer(data=mondai))
+button3.bind("<Button-1>", updateQuestion(data=mondai))
 
 
 # レイアウト
-area_question.pack()
+question_frame.pack()
 question.pack()
-
-area_image.pack()
-picture.pack()
-
-area_hints.pack()
 hint1.pack()
 hint2.pack()
-hint3.pack()
+picture.pack()
 
-area_text.pack()
-answer.pack()
+text_frame.pack()
+textarea.pack()
 
-area_answer.pack()
-button1.pack()
-button2.pack()
+answer_frame.pack()
+button1.pack(pady=10, padx=60, side='left')
+button2.pack(padx=60, side='left')
+button3.pack(padx=60, side='left')
+
+updateQuestion(question)
 
 # ウィンドウの表示開始
 root.mainloop()
